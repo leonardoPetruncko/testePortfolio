e003f2d8a231fbc78569a64ce61fc12dade6b4ed
leonardoPetrunko <leonardopetruncko20@gmail.com>
2022-02-17 16:42:54 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function showHome() {
+
+      document.getElementById("main").style.display = 'grid';
+      document.getElementById("profile").style.display = 'none';
+      
+}
+
+
+function showProfile() {
+      document.getElementById("main").style.display = 'none';
+      document.getElementById("profile").style.display = 'grid';
+      
+}
